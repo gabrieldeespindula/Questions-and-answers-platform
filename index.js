b6c8d0dbbe49ce26d8acd8ee4cf0e9e11464e44e
gabrieldeespindula <gabrieldeespindula80@gmail.com>
2021-03-29 20:49:09 -0300

    aks a questions base form

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get("/:name",(req, res) => {
+app.get("/",(req, res) => {
     var name = req.params.name
     res.render("index")
 });
+
+app.get("/ask",(req, res) => {
+    res.render("ask");
+})
 
 app.listen(8080,()=>{console.log("App is running!")});
