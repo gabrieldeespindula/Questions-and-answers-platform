3741d8ff2698607ced6088281a0ac01725283592
gabrieldeespindula <gabrieldeespindula80@gmail.com>
2021-03-31 19:10:57 -0300

    body-parser

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get("/",(req, res) => {
     var name = req.params.name
@@ -12,5 +16,9 @@ app.get("/",(req, res) => {
 app.get("/ask",(req, res) => {
     res.render("ask");
 })
+
+app.post("/saveAsk",(req, res) =>{
+
+});
 
 app.listen(8080,()=>{console.log("App is running!")});
