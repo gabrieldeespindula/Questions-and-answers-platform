6129f9e3a0409dad539ea510ec199dddecfce75f
gabrieldeespindula <gabrieldeespindula80@gmail.com>
2021-03-31 19:23:27 -0300

    using post method

--- index.js
+++ index.js
@@ -18,7 +18,9 @@ app.get("/ask",(req, res) => {
 })
 
 app.post("/saveAsk",(req, res) =>{
-
+    let title =  req.body.title;
+    let description = req.body.description;
+    res.send(title + description);
 });
 
 app.listen(8080,()=>{console.log("App is running!")});
