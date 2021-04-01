92440c7ed47dd211e85cb729ee895e4e142d9a38
gabrieldeespindula <gabrieldeespindula80@gmail.com>
2021-04-01 20:57:19 -0300

    create table questions if not exists

---
+++ database/Questions.js
@@ -0,0 +1,15 @@
+const Sequelize = require("sequelize");
+const connection =  require("./database");
+
+const Questions = connection.define('questions',{
+    title:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    description:{
+        type: Sequelize.TEXT,
+        allowNull: false
+    }
+}, {});
+
+Questions.sync({force: false}).then(() => {});
